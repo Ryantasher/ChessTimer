302eb1374d48ce4a37e6e20c0fda2d9e47116ecf
Ryan Asher <johndoe@example.com>
2023-01-11 22:46:19 -0500

    added comments to ChessTimerActivity

--- app/src/main/java/com/example/chesstimer/ChessTimerActivity.java
+++ app/src/main/java/com/example/chesstimer/ChessTimerActivity.java
@@ -10,24 +10,31 @@ import java.util.Locale;
 
 public class ChessTimerActivity extends AppCompatActivity {
 
+    //initalize timers
     private CountDownTimer countDownTimer1;
     private CountDownTimer countDownTimer2;
 
+    //initialize code interface to player layouts
     private TextView player1CountdownTextView;
     private TextView player2CountdownTextView;
 
-    //start time in seconds
+    //init player start times
     private long player1startTime = 300;
     private long player2startTime = 300;
+
+    //init player remaining times
     private long player1RemainingTime;
     private long player2RemainingTime;
 
+    //init player turn boolean
     private boolean isPlayer1TimerRunning;
     private boolean isPlayer2TimerRunning;
 
-    @Override
+
+    @Override //onCreate saves instance state in case of damage to activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //sets activity_main as the xml for the layout of this activity
         setContentView(R.layout.activity_main);
 
         //creates objects for the player layouts
@@ -87,19 +94,23 @@ public class ChessTimerActivity extends AppCompatActivity {
             }
         });
 
+        //player 2 listener
         player2CountdownTextView.setOnClickListener(new View.OnClickListener() {
             @Override
+            //when player2 layout is clicked
             public void onClick(View v) {
+                //if player2 isn't currently running when clicked (if clickable),
+                //start player2's clock, set player1 to un-clickable, and change the color to active
                 if (!isPlayer2TimerRunning) {
                     player2RemainingTime = player2startTime;
                     countDownTimer2 = new CountDownTimer(player2RemainingTime * 1000,
                             1000) {
-                        public void onTick(long millisUntilFinished) {
+                        public void onTick(long millisUntilFinished) { //updates time at tick
                             player2RemainingTime = millisUntilFinished / 1000;
                             player2CountdownTextView.setText(formatTime(player2RemainingTime));
                             player2startTime = player2RemainingTime;
                         }
-                        public void onFinish() {
+                        public void onFinish() { //prints text at finish
                             player2CountdownTextView.setText(R.string.TimeEnd);
                         }
                     }.start();
@@ -107,14 +118,14 @@ public class ChessTimerActivity extends AppCompatActivity {
                     player2CountdownTextView.setClickable(true);
                     player2CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
                     player1CountdownTextView.setClickable(false);
-                } else {
+                } else { //if player2 layout is clicked while it is already running, switch to
+                         // player1's clock, switch colors and clickable status
                     countDownTimer2.cancel();
                     isPlayer2TimerRunning = false;
                     player2CountdownTextView.setClickable(false);
                     player2CountdownTextView.setBackgroundColor(getResources().getColor(R.color.black));
                     player1CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
                     player1RemainingTime = player1startTime;
-
                     countDownTimer1 = new CountDownTimer(player1RemainingTime * 1000, 1000) {
                         public void onTick(long millisUntilFinished) {
                             player1RemainingTime = millisUntilFinished / 1000;
@@ -132,6 +143,7 @@ public class ChessTimerActivity extends AppCompatActivity {
         });
     }
 
+    //function to translate time to readable HH:MM:SS (want to update to be prettier)
     private String formatTime(long timeInSeconds) {
         int hours = (int) (timeInSeconds / 3600);
         int minutes = (int) ((timeInSeconds % 3600) / 60);
