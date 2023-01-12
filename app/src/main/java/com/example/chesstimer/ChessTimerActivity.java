f0ebaf7b4d80c6e6cc467b0db6bf0b769c9d4f80
Ryan Asher <johndoe@example.com>
2023-01-12 06:06:14 -0500

    Created functions for all timer interactions to increase code readability / ease of manipulation

--- app/src/main/java/com/example/chesstimer/ChessTimerActivity.java
+++ app/src/main/java/com/example/chesstimer/ChessTimerActivity.java
@@ -10,7 +10,7 @@ import java.util.Locale;
 
 public class ChessTimerActivity extends AppCompatActivity {
 
-    //initalize timers
+    //initialize timers
     private CountDownTimer countDownTimer1;
     private CountDownTimer countDownTimer2;
 
@@ -19,6 +19,7 @@ public class ChessTimerActivity extends AppCompatActivity {
     private TextView player2CountdownTextView;
 
     //init player start times
+    //maybe make 2 more time variables to track time updates, so that start times can be saved for reset?
     private long player1startTime = 300;
     private long player2startTime = 300;
 
@@ -49,47 +50,17 @@ public class ChessTimerActivity extends AppCompatActivity {
             @Override
             //when player1 layout is clicked
             public void onClick(View v) {
-                //if player1 isn't currently running when clicked (if clickable),
-                //start player1's clock, set player2 to un-clickable, and change the color to active
+                //if player1 isn't currently running when clicked (if clickable), start player1's clock
                 if (!isPlayer1TimerRunning) {
                     player1RemainingTime = player1startTime;
-                    countDownTimer1 = new CountDownTimer(player1RemainingTime * 1000,
-                            1000) {
-                        public void onTick(long millisUntilFinished) {
-                            player1RemainingTime = millisUntilFinished / 1000;
-                            player1CountdownTextView.setText(formatTime(player1RemainingTime));
-                            player1startTime = player1RemainingTime;
-                        }
-                        public void onFinish() {
-                            player1CountdownTextView.setText(R.string.TimeEnd);
-                        }
-                    }.start();
-                    isPlayer1TimerRunning=true;
-                    player1CountdownTextView.setClickable(true);
-                    player1CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
-                    player2CountdownTextView.setClickable(false);
+                    initTimer(countDownTimer1);
+                    startTimer(countDownTimer1);
 
-                } else { //if player1 layout is clicked while it is already running, switch to
-                         // player2's clock, switch colors and clickable status
-                    countDownTimer1.cancel();
-                    isPlayer1TimerRunning = false;
-                    player1CountdownTextView.setClickable(false);
-                    player2CountdownTextView.setClickable(true);
-                    player1CountdownTextView.setBackgroundColor(getResources().getColor(R.color.black));
-                    player2CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                } else { //if player1 layout is clicked while it is already running, switch to player2's clock
+                    pauseTimer(countDownTimer1);
                     player2RemainingTime = player2startTime;
-                    countDownTimer2 = new CountDownTimer(player2RemainingTime * 1000,
-                            1000) {
-                        public void onTick(long millisUntilFinished) {
-                            player2RemainingTime = millisUntilFinished / 1000;
-                            player2CountdownTextView.setText(formatTime(player2RemainingTime));
-                            player2startTime = player2RemainingTime;
-                        }
-                        public void onFinish() {
-                            player2CountdownTextView.setText(R.string.TimeEnd);
-                        }
-                    }.start();
-                    isPlayer2TimerRunning = true;
+                    initTimer(countDownTimer2);
+                    startTimer(countDownTimer2);
                 }
             }
         });
@@ -99,45 +70,16 @@ public class ChessTimerActivity extends AppCompatActivity {
             @Override
             //when player2 layout is clicked
             public void onClick(View v) {
-                //if player2 isn't currently running when clicked (if clickable),
-                //start player2's clock, set player1 to un-clickable, and change the color to active
+                //if player2 isn't currently running when clicked (if clickable), start player2's clock
                 if (!isPlayer2TimerRunning) {
                     player2RemainingTime = player2startTime;
-                    countDownTimer2 = new CountDownTimer(player2RemainingTime * 1000,
-                            1000) {
-                        public void onTick(long millisUntilFinished) { //updates time at tick
-                            player2RemainingTime = millisUntilFinished / 1000;
-                            player2CountdownTextView.setText(formatTime(player2RemainingTime));
-                            player2startTime = player2RemainingTime;
-                        }
-                        public void onFinish() { //prints text at finish
-                            player2CountdownTextView.setText(R.string.TimeEnd);
-                        }
-                    }.start();
-                    isPlayer2TimerRunning = true;
-                    player2CountdownTextView.setClickable(true);
-                    player2CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
-                    player1CountdownTextView.setClickable(false);
-                } else { //if player2 layout is clicked while it is already running, switch to
-                         // player1's clock, switch colors and clickable status
-                    countDownTimer2.cancel();
-                    isPlayer2TimerRunning = false;
-                    player2CountdownTextView.setClickable(false);
-                    player2CountdownTextView.setBackgroundColor(getResources().getColor(R.color.black));
-                    player1CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                    initTimer(countDownTimer2);
+                    startTimer(countDownTimer2);
+                } else { //if player2 layout is clicked while it is already running, switch to player1's clock
+                    pauseTimer(countDownTimer2);
                     player1RemainingTime = player1startTime;
-                    countDownTimer1 = new CountDownTimer(player1RemainingTime * 1000, 1000) {
-                        public void onTick(long millisUntilFinished) {
-                            player1RemainingTime = millisUntilFinished / 1000;
-                            player1CountdownTextView.setText(formatTime(player1RemainingTime));
-                            player1startTime = player1RemainingTime;
-                        }
-                        public void onFinish() {
-                            player1CountdownTextView.setText(R.string.TimeEnd);
-                        }
-                    }.start();
-                    isPlayer1TimerRunning = true;
-                    player1CountdownTextView.setClickable(true);
+                    initTimer(countDownTimer1);
+                    startTimer(countDownTimer1);
                 }
             }
         });
@@ -149,7 +91,82 @@ public class ChessTimerActivity extends AppCompatActivity {
         int minutes = (int) ((timeInSeconds % 3600) / 60);
         int seconds = (int) (timeInSeconds % 60);
 
+        if (hours == 0) {
+            if (minutes < 10) {
+                return String.format(Locale.getDefault(), "%1d:%02d", minutes, seconds);
+            }
+            return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        }
         return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    //function that creates a new CountDownTimer object based on the player
+    private void initTimer(CountDownTimer cdt) {
+        if (cdt.equals(countDownTimer1)) {
+            countDownTimer1 = new CountDownTimer(player1RemainingTime * 1000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    player1RemainingTime = millisUntilFinished / 1000;
+                    player1CountdownTextView.setText(formatTime(player1RemainingTime));
+                    player1startTime = player1RemainingTime;
+                }
+
+                @Override
+                public void onFinish() {
+                    player1CountdownTextView.setText(R.string.TimeEnd);
+                }
+            };
+        } else {
+            countDownTimer2 = new CountDownTimer(player2RemainingTime * 1000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    player2RemainingTime = millisUntilFinished / 1000;
+                    player2CountdownTextView.setText(formatTime(player2RemainingTime));
+                    player2startTime = player2RemainingTime;
+                }
+
+                @Override
+                public void onFinish() {
+                    player2CountdownTextView.setText(R.string.TimeEnd);
+                }
+            };
+        }
+    }
+
+    //function that starts a CountDownTimer object based on the player
+    private void startTimer(CountDownTimer cdt) {
+        if (cdt.equals(countDownTimer1)) {
+            cdt.start();
+            isPlayer1TimerRunning=true;
+            player1CountdownTextView.setClickable(true);
+            player2CountdownTextView.setClickable(false);
+            player1CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        } else {
+            cdt.start();
+            isPlayer2TimerRunning=true;
+            player2CountdownTextView.setClickable(true);
+            player1CountdownTextView.setClickable(false);
+            player2CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        }
+    }
+
+    //function that stops a CountDownTimer object based on the player
+    private void pauseTimer(CountDownTimer cdt) {
+        if (cdt.equals(countDownTimer1)) {
+            cdt.cancel();
+            isPlayer1TimerRunning = false;
+            player1CountdownTextView.setClickable(false);
+            player2CountdownTextView.setClickable(true);
+            player1CountdownTextView.setBackgroundColor(getResources().getColor(R.color.black));
+            player2CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        } else {
+            cdt.cancel();
+            isPlayer2TimerRunning = false;
+            player2CountdownTextView.setClickable(false);
+            player1CountdownTextView.setClickable(true);
+            player2CountdownTextView.setBackgroundColor(getResources().getColor(R.color.black));
+            player1CountdownTextView.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        }
     }
 
 }
